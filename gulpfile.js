3df3875c3e31c05b84b6d5ed2117664f8e9cb9d5
FacelessWolf <alex.sakovskii.92@gmail.com>
2020-05-12 04:20:20 +0300

    upd v12.5.2020

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 let project_folder = require('path').basename(__dirname + '_project');
 let source_folder = '#source';
 
+let fileSystem = require('fs')
+
 let path = {
 	build: {
 		html: project_folder + '/',
@@ -48,7 +50,10 @@ let { src, dest } = require('gulp'),
 	img_min = require('gulp-imagemin'),
 	webp = require('gulp-webp'),
 	webp_html = require('gulp-webp-html'),
-	webp_css = require('gulp-webpcss');
+	webp_css = require('gulp-webpcss'),
+	ttf2woff = require('gulp-ttf2woff'),
+	ttf2woff2 = require('gulp-ttf2woff2'),
+	otf2ttf = require('gulp-fonter');
 //svg_sprite = require('gulp-svg-sprite');
 
 function browserSync(params) {
@@ -146,6 +151,39 @@ function readyJsCopy() {
 		.pipe(browser_sync.stream())
 }
 
+function fonts() {
+	src(path.source.fonts)
+		.pipe(ttf2woff())
+		.pipe(dest(path.build.fonts));
+	return src(path.source.fonts)
+		.pipe(ttf2woff2())
+		.pipe(dest(path.build.fonts));
+}
+
+function fontsConn(params) {
+	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+	if (file_content == '') {
+		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+		return fs.readdir(path.build.fonts, function (err, items) {
+			if (items) {
+				let c_fontname;
+				for (var i = 0; i < items.length; i++) {
+					let fontname = items[i].split('.');
+					fontname = fontname[0];
+					if (c_fontname != fontname) {
+						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+					}
+					c_fontname = fontname;
+				}
+			}
+		})
+	}
+}
+
+function cb() {
+
+}
+
 function watchFiles(params) {
 	gulp.watch([path.watch.html], html);
 	gulp.watch([path.watch.css], css);
@@ -157,13 +195,23 @@ function cleaning(params) {
 	return del(path.clean)
 }
 
-let building_project = gulp.series(cleaning, gulp.parallel(images, js, css, html, readyCssCopy, readyJsCopy), minMainCss);
+gulp.task('otf2ttf', function () {
+	return src([source_folder + '/fonts/*.otf'])
+		.pipe(otf2ttf({
+			formats: ['ttf']
+		}))
+		.pipe(dest(source_folder + '/fonts/'));
+})
+
+let building_project = gulp.series(cleaning, gulp.parallel(images, js, css, html, fonts, readyCssCopy, readyJsCopy), fontsConn);
 let watch = gulp.parallel(building_project, watchFiles, browserSync);
 
+exports.fontsConn = fontsConn;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
-exports.build = building_project;
+exports.building_project = building_project;
 exports.watch = watch;
 exports.default = watch;
